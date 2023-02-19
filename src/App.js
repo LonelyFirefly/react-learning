b993115f9bcecff14a24e0d8fa62670eec9c8690
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-19 19:38:40 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,63 +1,49 @@
 import { useState } from "react";
 
-export default function Form() {
-	const [answer, setAnswer] = useState("");
-	const [error, setError] = useState(null);
-	const [status, setStatus] = useState("typing"); // typing/submittig/success
+const initialItems = [
+	{ title: "pretzels", id: 0 },
+	{ title: "crispy seaweed", id: 1 },
+	{ title: "granola bar", id: 2 },
+];
 
-	if (status === "success") {
-		return <h1>That's right</h1>;
-	}
+export default function FeedbackForm() {
+	const [items, setItems] = useState(initialItems);
+	const [selectedItem, setSelectedItem] = useState(items[0]);
 
-	async function handleSubmit(e) {
-		e.preventDefault();
-		setStatus("submitting");
-		try {
-			await submitForm(answer);
-			setStatus("success");
-		} catch (err) {
-			setStatus("typing");
-			setError(err);
-		}
-	}
-
-	function handleTextAreaChange(e) {
-		setAnswer(e.target.value);
+	function handleItemChange(id, e) {
+		setItems(
+			items.map((item) => {
+				if (item.id === id) {
+					return { ...item, title: e.target.value };
+				} else {
+					return item;
+				}
+			})
+		);
 	}
 
 	return (
 		<>
-			<h2>City Quiz</h2>
-			<p>
-				In which city is there a billboard that turns air into drinkable
-				water?
-			</p>
-			<form onSubmit={handleSubmit}>
-				<textarea
-					value={answer}
-					onChange={handleTextAreaChange}
-					disabled={status === "submitting"}
-				/>
-				<br />
-				<button
-					disabled={answer.length === 0 || status === "submitting"}>
-					Submit
-				</button>
-				{error !== null && <p className="Error">{error.message}</p>}
-			</form>
+			<h2>What's your travel snack?</h2>
+			<ul>
+				{items.map((item, index) => (
+					<li key={item.id}>
+						<input
+							value={item.title}
+							onChange={(e) => {
+								handleItemChange(item.id, e);
+							}}
+						/>
+						<button
+							onClick={() => {
+								setSelectedItem(item);
+							}}>
+							Choose
+						</button>
+					</li>
+				))}
+			</ul>
+			<p>You picked {selectedItem.title}</p>
 		</>
 	);
-}
-
-function submitForm(answer) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			let shouldError = answer.toLowerCase() !== "lima";
-			if (shouldError) {
-				reject(new Error("Good guess but a wrong answer. Try again!"));
-			} else {
-				resolve();
-			}
-		}, 1500);
-	});
 }
