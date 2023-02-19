aca020310de79d843e868ff5e12cf6e2c5e2b707
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-19 18:55:08 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,74 +1,63 @@
-import { useImmer } from "use-immer";
+import { useState } from "react";
 
 export default function Form() {
-	const [person, updatePerson] = useImmer({
-		name: "Niki de Saint Phalle",
-		artwork: {
-			title: "Blue Nana",
-			city: "Hamburg",
-			image: "https://i.imgur.com/Sd1AgUOm.jpg",
-		},
-	});
+	const [answer, setAnswer] = useState("");
+	const [error, setError] = useState(null);
+	const [status, setStatus] = useState("typing"); // typing/submittig/success
 
-	function handleNameChange(e) {
-		updatePerson((draft) => {
-			draft.name = e.target.value;
-		});
+	if (status === "success") {
+		return <h1>That's right</h1>;
 	}
 
-	function handleTitleChange(e) {
-		updatePerson((draft) => {
-			draft.artwork.title = e.target.value;
-		});
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setStatus("submitting");
+		try {
+			await submitForm(answer);
+			setStatus("success");
+		} catch (err) {
+			setStatus("typing");
+			setError(err);
+		}
 	}
 
-	function handleCityChange(e) {
-		updatePerson((draft) => {
-			draft.artwork.city = e.target.value;
-		});
-	}
-
-	function handleImageChange(e) {
-		updatePerson((draft) => {
-			draft.artwork.image = e.target.value;
-		});
+	function handleTextAreaChange(e) {
+		setAnswer(e.target.value);
 	}
 
 	return (
 		<>
-			<label>
-				Name:
-				<input value={person.name} onChange={handleNameChange} />
-			</label>
-			<label>
-				Title:
-				<input
-					value={person.artwork.title}
-					onChange={handleTitleChange}
-				/>
-			</label>
-			<label>
-				City:
-				<input
-					value={person.artwork.city}
-					onChange={handleCityChange}
-				/>
-			</label>
-			<label>
-				Image:
-				<input
-					value={person.artwork.image}
-					onChange={handleImageChange}
-				/>
-			</label>
+			<h2>City Quiz</h2>
 			<p>
-				<i>{person.artwork.title}</i>
-				{" by "}
-				{person.name}
-				<br />
-				(located in {person.artwork.city})
+				In which city is there a billboard that turns air into drinkable
+				water?
 			</p>
-			<img src={person.artwork.image} alt={person.artwork.title} />
+			<form onSubmit={handleSubmit}>
+				<textarea
+					value={answer}
+					onChange={handleTextAreaChange}
+					disabled={status === "submitting"}
+				/>
+				<br />
+				<button
+					disabled={answer.length === 0 || status === "submitting"}>
+					Submit
+				</button>
+				{error !== null && <p className="Error">{error.message}</p>}
+			</form>
 		</>
 	);
+}
+
+function submitForm(answer) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			let shouldError = answer.toLowerCase() !== "lima";
+			if (shouldError) {
+				reject(new Error("Good guess but a wrong answer. Try again!"));
+			} else {
+				resolve();
+			}
+		}, 1500);
+	});
 }
