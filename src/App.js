b41c213260e0c6477f8b4c206f4d6bdf398d6b20
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-21 20:45:00 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,70 +1,42 @@
 import { useState } from "react";
-import { initialTravelPlan } from "./places";
+import ContactList from "./Components/ContactList";
+import EditContact from "./Components/EditContact";
 
-export default function TravelPlan() {
-	const [plan, setPlan] = useState(initialTravelPlan);
+const initialContacts = [
+	{ id: 0, name: "Taylor", email: "taylor@mail.com" },
+	{ id: 1, name: "Alice", email: "alice@mail.com" },
+	{ id: 2, name: "Bob", email: "bob@mail.com" },
+];
 
-	function handleComplete(parentId, childId) {
-		const parent = plan[parentId];
-		// Create a new version of the parent place
-		// that doesn't include this child ID.
-		const nextParent = {
-			...parent,
-			childIds: parent.childIds.filter((id) => id !== childId),
-		};
-		// Update the root state object...
-		setPlan({
-			...plan,
-			// ...so that it has the updated parent.
-			[parentId]: nextParent,
+export default function ContactManager() {
+	const [contacts, setContacts] = useState(initialContacts);
+	const [selectedId, setSelectedId] = useState(0);
+	const selectedContact = contacts.find((c) => c.id === selectedId);
+
+	function handleSave(updatedData) {
+		const nextContacts = contacts.map((c) => {
+			if (c.id === updatedData.id) {
+				return updatedData;
+			} else {
+				return c;
+			}
 		});
+		setContacts(nextContacts);
 	}
 
-	const root = plan[0];
-	const planetIds = root.childIds;
 	return (
-		<>
-			<h2>Places to visit</h2>
-			<ol>
-				{planetIds.map((id) => (
-					<PlaceTree
-						key={id}
-						id={id}
-						parentId={0}
-						placesById={plan}
-						onComplete={handleComplete}
-					/>
-				))}
-			</ol>
-		</>
-	);
-}
-
-function PlaceTree({ id, parentId, placesById, onComplete }) {
-	const place = placesById[id];
-	const childIds = place.childIds;
-	return (
-		<li>
-			{place.title}
-			<button
-				onClick={() => {
-					onComplete(parentId, id);
-				}}>
-				Complete
-			</button>
-			{childIds.length > 0 && (
-				<ol>
-					{childIds.map((childId) => (
-						<PlaceTree
-							key={childId}
-							id={childId}
-							parentId={id}
-							placesById={placesById}
-							onComplete={onComplete}
-						/>
-					))}
-				</ol>
-			)}
-		</li>
+		<div>
+			<ContactList
+				contacts={contacts}
+				selectedId={selectedId}
+				onSelect={(id) => setSelectedId(id)}
+			/>
+			<hr />
+			<EditContact
+				key={selectedId}
+				initialData={selectedContact}
+				onSave={handleSave}
+			/>
+		</div>
 	);
 }
