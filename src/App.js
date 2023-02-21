efa00a975a1f1d10f28b35b76a33800c83abf2c4
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-21 13:14:22 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
 import { useState } from "react";
+import { initialTravelPlan } from "./places";
 
-const initialItems = [
-	{ title: "pretzels", id: 0 },
-	{ title: "crispy seaweed", id: 1 },
-	{ title: "granola bar", id: 2 },
-];
+export default function TravelPlan() {
+	const [plan, setPlan] = useState(initialTravelPlan);
 
-export default function FeedbackForm() {
-	const [items, setItems] = useState(initialItems);
-	const [selectedItem, setSelectedItem] = useState(items[0]);
-
-	function handleItemChange(id, e) {
-		setItems(
-			items.map((item) => {
-				if (item.id === id) {
-					return { ...item, title: e.target.value };
-				} else {
-					return item;
-				}
-			})
-		);
+	function handleComplete(parentId, childId) {
+		const parent = plan[parentId];
+		// Create a new version of the parent place
+		// that doesn't include this child ID.
+		const nextParent = {
+			...parent,
+			childIds: parent.childIds.filter((id) => id !== childId),
+		};
+		// Update the root state object...
+		setPlan({
+			...plan,
+			// ...so that it has the updated parent.
+			[parentId]: nextParent,
+		});
 	}
 
+	const root = plan[0];
+	const planetIds = root.childIds;
 	return (
 		<>
-			<h2>What's your travel snack?</h2>
-			<ul>
-				{items.map((item, index) => (
-					<li key={item.id}>
-						<input
-							value={item.title}
-							onChange={(e) => {
-								handleItemChange(item.id, e);
-							}}
-						/>
-						<button
-							onClick={() => {
-								setSelectedItem(item);
-							}}>
-							Choose
-						</button>
-					</li>
+			<h2>Places to visit</h2>
+			<ol>
+				{planetIds.map((id) => (
+					<PlaceTree
+						key={id}
+						id={id}
+						parentId={0}
+						placesById={plan}
+						onComplete={handleComplete}
+					/>
 				))}
-			</ul>
-			<p>You picked {selectedItem.title}</p>
+			</ol>
 		</>
+	);
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+	const place = placesById[id];
+	const childIds = place.childIds;
+	return (
+		<li>
+			{place.title}
+			<button
+				onClick={() => {
+					onComplete(parentId, id);
+				}}>
+				Complete
+			</button>
+			{childIds.length > 0 && (
+				<ol>
+					{childIds.map((childId) => (
+						<PlaceTree
+							key={childId}
+							id={childId}
+							parentId={id}
+							placesById={placesById}
+							onComplete={onComplete}
+						/>
+					))}
+				</ol>
+			)}
+		</li>
 	);
 }
