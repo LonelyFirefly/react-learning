600e8cbdb1f3da15964f09db510faeba49d267ab
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-22 13:28:48 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import { useState } from "react";
-import ContactList from "./Components/ContactList";
-import EditContact from "./Components/EditContact";
+import Heading from "./Components/Heading.js";
+import Section from "./Components/Section.js";
 
-const initialContacts = [
-	{ id: 0, name: "Taylor", email: "taylor@mail.com" },
-	{ id: 1, name: "Alice", email: "alice@mail.com" },
-	{ id: 2, name: "Bob", email: "bob@mail.com" },
-];
-
-export default function ContactManager() {
-	const [contacts, setContacts] = useState(initialContacts);
-	const [selectedId, setSelectedId] = useState(0);
-	const selectedContact = contacts.find((c) => c.id === selectedId);
-
-	function handleSave(updatedData) {
-		const nextContacts = contacts.map((c) => {
-			if (c.id === updatedData.id) {
-				return updatedData;
-			} else {
-				return c;
-			}
-		});
-		setContacts(nextContacts);
-	}
-
+export default function Page() {
 	return (
-		<div>
-			<ContactList
-				contacts={contacts}
-				selectedId={selectedId}
-				onSelect={(id) => setSelectedId(id)}
-			/>
-			<hr />
-			<EditContact
-				key={selectedId}
-				initialData={selectedContact}
-				onSave={handleSave}
-			/>
-		</div>
+		<Section>
+			<Heading>Title</Heading>
+			<Section>
+				<Heading>Heading</Heading>
+				<Heading>Heading</Heading>
+				<Heading>Heading</Heading>
+				<Section>
+					<Heading>Sub-heading</Heading>
+					<Heading>Sub-heading</Heading>
+					<Heading>Sub-heading</Heading>
+					<Section>
+						<Heading>Sub-sub-heading</Heading>
+						<Heading>Sub-sub-heading</Heading>
+						<Heading>Sub-sub-heading</Heading>
+					</Section>
+				</Section>
+			</Section>
+		</Section>
 	);
 }
