2c4366e20d97a6cb47f490bbe334df28df654c20
LonelyFirefly <Itisperfectworld@gmail.com>
2023-02-26 21:47:01 +0400

    new struggles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import Heading from "./Components/Heading.js";
-import Section from "./Components/Section.js";
+import { useEffect, useState, useSyncExternalStore } from "react";
 
-export default function Page() {
-	return (
-		<Section>
-			<Heading>Title</Heading>
-			<Section>
-				<Heading>Heading</Heading>
-				<Heading>Heading</Heading>
-				<Heading>Heading</Heading>
-				<Section>
-					<Heading>Sub-heading</Heading>
-					<Heading>Sub-heading</Heading>
-					<Heading>Sub-heading</Heading>
-					<Section>
-						<Heading>Sub-sub-heading</Heading>
-						<Heading>Sub-sub-heading</Heading>
-						<Heading>Sub-sub-heading</Heading>
-					</Section>
-				</Section>
-			</Section>
-		</Section>
+function subscribe(callback) {
+	window.addEventListener("online", callback);
+	window.addEventListener("offline", callback);
+
+	return () => {
+		window.removeEventListener("online", callback);
+		window.removeEventListener("offline", callback);
+	};
+}
+
+export default function useOnlineStatus() {
+	return useSyncExternalStore(
+		subscribe,
+		() => navigator.onLine,
+		() => true
 	);
+}
+
+function ChatIndicator() {
+	const isOnline = useOnlineStatus();
 }
